cefcef081883382a23efe7c680d8ef748ec04771
Robert Brook <robertbrook@fastmail.fm>
2020-02-20 16:50:05 +0000

    testing ttlurl

--- app.py
+++ app.py
@@ -6,15 +6,36 @@ from string import Template
 from flask import Flask, escape, request, render_template
 app = Flask(__name__)
 
-@app.route("/")
-@app.route('/fromurl/<ttlurl>')
-def hello(ttlurl=None):
-    return render_template('ttl.html', ttlurl=ttlurl, title="title",
-    description="description",
-    classes="classes",
-    properties="properties",
-    namespaces="namespaces",
-    makers="makers")
+
+    
+@app.route('/')
+def index():
+
+	ttlurl = request.args.get('ttlurl', "https://raw.githubusercontent.com/ukparliament/ontologies/master/procedure/procedure-ontology.ttl")
+	
+	ppr = rdflib.Namespace("http://parliament.uk/ontologies/procedure/")
+	g = rdflib.Graph()
+	
+	result = g.parse(data=requests.get(ttlurl).text, format="turtle")
+	
+	classes = []
+
+	for s, p, o in g.triples((None, RDF.type, OWL.Class)):
+		isSubClass = g.value(s, RDFS.subClassOf)
+		subClassNote = ""
+		if isSubClass is not None:
+			subClassNote = f"<p>This class is a <strong>subclass</strong> of {g.value(s, RDFS.subClassOf)}.</p>"
+
+		classes.append(f'<article><h3>{g.label(s)}</h3> <p>{g.value(s, RDFS.comment)}</p>{subClassNote}</article>')
+
+	classes = ''.join(classes)
+	
+	return render_template('ttl.html', title="title",
+	description="description",
+	classes=classes,
+	properties="properties",
+	namespaces="namespaces",
+	makers="TK makers")
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
