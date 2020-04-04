2cf9d553f0f8f91557460b874bd850945545388e
Robert Brook <robertbrook@fastmail.fm>
2020-04-04 20:43:35 +0100

    Interface ttl filter

--- app.py
+++ app.py
@@ -8,111 +8,124 @@ from flask import Flask, escape, request, render_template, flash
 from markupsafe import Markup
 
 app = Flask(__name__)
-app.jinja_options['extensions'].append('jinja2.ext.debug')
+app.jinja_options["extensions"].append("jinja2.ext.debug")
 app.secret_key = b'*Y}P;s$&9*hDVw&f4KyXR,v.]cG/m_x>&9TQQ?:t!"'
 
 root_url = "https://ukparliament.github.io/ontologies/"
 
 fallback_url = "https://ukparliament.github.io/ontologies/interface/interface.ttl"
 
-@app.route('/')
+
+@app.route("/")
 def index():
 
-	ttlurl = request.args.get('ttlurl', fallback_url)
-	
-	if not ttlurl.startswith(root_url):
-		ttlurl = fallback_url
-		flash("ttl2html needs an address that starts with " + root_url)
-		flash("Here's the interface ontology instead.")
-		
-	if not ttlurl.endswith(".ttl"):
-		ttlurl = fallback_url
-		flash("ttl2html needs a .ttl file.")
-		flash("Here's the interface ontology instead.")
+    ttlurl = request.args.get("ttlurl", fallback_url)
 
-	g = rdflib.Graph()
-	
-	result = g.parse(data=requests.get(ttlurl).text, format="turtle")
-	
-# 	for s, p, o in g.triples((None, OWL.imports, None)):
-# 		print(o)
-	
-	classes = []
+    if not ttlurl.startswith(root_url):
+        ttlurl = fallback_url
+        flash("ttl2html needs an address that starts with " + root_url)
+        flash("Here's the interface ontology instead.")
 
-	for s, p, o in g.triples((None, RDF.type, OWL.Class)):		
-		superclasses = []
-		
-		superclassobjects = g.objects(s, RDFS.subClassOf)
-		for superclassobject in superclassobjects:
-			superclasses.append(superclassobject.split('/')[-1])
+    if not ttlurl.endswith(".ttl"):
+        ttlurl = fallback_url
+        flash("ttl2html needs a .ttl file.")
+        flash("Here's the interface ontology instead.")
 
-		h3id = g.label(s).lower().replace(" ", "-")
-#		  classes.append(f'<article class="class"><h3 id="{h3id}">{g.label(s)}</h3> {subClassNote}<p>{g.value(s, RDFS.comment)}</p></article>')
-		classes.append({'label':g.label(s), 'comment':g.value(s, RDFS.comment), 'superclasses':superclasses})
-	
-	objectproperties = []
+    g = rdflib.Graph()
 
-	for s, p, o in g.triples((None, RDF.type, OWL.ObjectProperty)):
-		h3id = g.label(s).lower().replace(" ", "-")
-		domainstub = g.value(s, RDFS.domain).split('/')[-1]
-		rangestub = g.value(s, RDFS.range).split('/')[-1]
-		superpropertyv = g.value(s, RDFS.subPropertyOf)
-		if superpropertyv:
-			superproperty = g.value(s, RDFS.subPropertyOf).split('/')[-1]
-		else:
-			superproperty = ""
+    result = g.parse(data=requests.get(ttlurl).text, format="turtle")
 
-# 		properties.append(f'<article class="property"><h3 id="{h3id}">{g.label(s)}</h3><ul><li>{domainstub} (domain) &rarr; {g.label(s)} (property) &rarr; {rangestub} (range)</li></ul><p>{g.value(s, RDFS.comment)}</p></article>')
-		objectproperties.append({'label':g.label(s), 'domain':domainstub, 'range':rangestub, 'superproperty':superproperty, 'comment':g.value(s, RDFS.comment)})
+    classes = []
 
-	for s, p, o in g.triples((None, RDF.type, OWL.Ontology)):
-		title = g.value(s, DCTERMS.title)
-		description = g.value(s, DCTERMS.description)
-		created = g.value(s, DCTERMS.created)
-		rights = g.value(s, DCTERMS.rights)
-		depiction = g.value(s, FOAF.depiction) or "" # check		
-	
-	foafmakerids = []
-	
-	makerobjects = []
-	
-	for maker in g.objects(s, FOAF.maker):
-		foafmakerids.append(maker)
-		makerobject = {}
-		for s,p,o in g.triples((maker,None,None)):
-			makerobject['id'] = str(s)
-			makerobject['name'] = str(g.value(s, FOAF.name))
-			makerobject['homepage'] = str(g.value(s, FOAF.homepage))
-				
-			makerobjects.append(makerobject)
-			
-	makers = []
-	for i in makerobjects:
-		if i not in makers:
-			makers.append(i)
+    for s, p, o in g.triples((None, RDF.type, OWL.Class)):
+        superclasses = []
 
-	return render_template('index.html', 
-	title=Markup(title),
-	created=Markup(created),
-	rights=Markup(rights),
-	description=Markup(description),
-	depiction=Markup(depiction),
-	ttlurl=ttlurl,
-	classes=classes,
-	objectproperties=objectproperties,
-	namespaces=g.namespaces(),
-	makers = makers,
-	root_url=root_url,
-	imports=g.triples((None, OWL.imports, None)),
-	equivalentClasses=g.triples((None, OWL.equivalentClass, None)),
-	subClasses=g.triples((None, RDFS.subClassOf, None)),
-	importsum=sum(1 for _ in g.triples((None, OWL.imports, None)))
-	)
+        superclassobjects = g.objects(s, RDFS.subClassOf)
+        for superclassobject in superclassobjects:
+            superclasses.append(superclassobject.split("/")[-1])
+
+        h3id = g.label(s).lower().replace(" ", "-")
+        # 		  classes.append(f'<article class="class"><h3 id="{h3id}">{g.label(s)}</h3> {subClassNote}<p>{g.value(s, RDFS.comment)}</p></article>')
+        classes.append(
+            {
+                "label": g.label(s),
+                "comment": g.value(s, RDFS.comment),
+                "superclasses": superclasses,
+            }
+        )
+
+    objectproperties = []
+
+    for s, p, o in g.triples((None, RDF.type, OWL.ObjectProperty)):
+        h3id = g.label(s).lower().replace(" ", "-")
+        domainstub = g.value(s, RDFS.domain).split("/")[-1]
+        rangestub = g.value(s, RDFS.range).split("/")[-1]
+        superpropertyv = g.value(s, RDFS.subPropertyOf)
+        if superpropertyv:
+            superproperty = g.value(s, RDFS.subPropertyOf).split("/")[-1]
+        else:
+            superproperty = ""
+
+        # 		properties.append(f'<article class="property"><h3 id="{h3id}">{g.label(s)}</h3><ul><li>{domainstub} (domain) &rarr; {g.label(s)} (property) &rarr; {rangestub} (range)</li></ul><p>{g.value(s, RDFS.comment)}</p></article>')
+        objectproperties.append(
+            {
+                "label": g.label(s),
+                "domain": domainstub,
+                "range": rangestub,
+                "superproperty": superproperty,
+                "comment": g.value(s, RDFS.comment),
+            }
+        )
+
+    for s, p, o in g.triples((None, RDF.type, OWL.Ontology)):
+        title = g.value(s, DCTERMS.title)
+        description = g.value(s, DCTERMS.description)
+        created = g.value(s, DCTERMS.created)
+        rights = g.value(s, DCTERMS.rights)
+        depiction = g.value(s, FOAF.depiction) or ""  # check
+
+    foafmakerids = []
+
+    makerobjects = []
+
+    for maker in g.objects(s, FOAF.maker):
+        foafmakerids.append(maker)
+        makerobject = {}
+        for s, p, o in g.triples((maker, None, None)):
+            makerobject["id"] = str(s)
+            makerobject["name"] = str(g.value(s, FOAF.name))
+            makerobject["homepage"] = str(g.value(s, FOAF.homepage))
+
+            makerobjects.append(makerobject)
+
+    makers = []
+    for i in makerobjects:
+        if i not in makers:
+            makers.append(i)
+
+    return render_template(
+        "index.html",
+        title=Markup(title),
+        created=Markup(created),
+        rights=Markup(rights),
+        description=Markup(description),
+        depiction=Markup(depiction),
+        ttlurl=ttlurl,
+        classes=classes,
+        objectproperties=objectproperties,
+        namespaces=g.namespaces(),
+        makers=makers,
+        root_url=root_url,
+        imports=g.triples((None, OWL.imports, None)),
+        equivalentClasses=g.triples((None, OWL.equivalentClass, None)),
+        subClasses=g.triples((None, RDFS.subClassOf, None)),
+        importsum=sum(1 for _ in g.triples((None, OWL.imports, None))),
+    )
+
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 # data=requests.get(githuburl).text
-
